e5467b0009e47bd8d611d2d274e83a60ca2c5e18
Afika2021 <vlad.merzlyakov.89@gmail.com>
2021-02-11 09:48:45 +0400

    pract-11-02-21

---
+++ проект-v-7/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Uighur", 18.25F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Qest #10";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Uighur", 18.25F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(241, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(223, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Qest #20";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.listBox2);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Location = new System.Drawing.Point(775, 99);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 367);
+            this.panel1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(0, 299);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(773, 65);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Генирация события";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.label1.Location = new System.Drawing.Point(353, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "==";
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.SystemColors.MenuText;
+            this.listBox2.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox2.ForeColor = System.Drawing.Color.Lime;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 19;
+            this.listBox2.Location = new System.Drawing.Point(425, 3);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(348, 289);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.listBox1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.ForeColor = System.Drawing.Color.Lime;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(348, 289);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.listBox3);
+            this.panel2.Controls.Add(this.listBox4);
+            this.panel2.Location = new System.Drawing.Point(12, 42);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(776, 367);
+            this.panel2.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(0, 299);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(773, 65);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Генирация события";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.label2.Location = new System.Drawing.Point(353, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 46);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "==";
+            // 
+            // listBox3
+            // 
+            this.listBox3.BackColor = System.Drawing.SystemColors.MenuText;
+            this.listBox3.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox3.ForeColor = System.Drawing.Color.Lime;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 19;
+            this.listBox3.Location = new System.Drawing.Point(425, 3);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(348, 289);
+            this.listBox3.TabIndex = 1;
+            // 
+            // listBox4
+            // 
+            this.listBox4.BackColor = System.Drawing.SystemColors.MenuText;
+            this.listBox4.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox4.ForeColor = System.Drawing.Color.Lime;
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 19;
+            this.listBox4.Location = new System.Drawing.Point(3, 3);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(348, 289);
+            this.listBox4.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+    }
+}
+
---
+++ проект-v-7/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double[] Q = new double[20];
+            Random rand = new Random();
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            for (int i = 0; i < 20; i++)
+            {
+                Q[i] = rand.Next(0, 100);
+                listBox1.Items.Insert(listBox1.SelectedItems.Count, "Q[" + i +"] = '" + Q[i] + "'");
+            }
+            for (int i = 0; i < 20; i++)
+            {
+               if ((Q[i] / 7 == 5) || (Q[i] / 7 == 1) || (Q[i] / 7 == 2))
+                {
+                    listBox2.Items.Insert(listBox1.SelectedItems.Count, "Q[" + i + "] = '" + Q[i] + "'");
+                }
+               
+            }
+            if (listBox2.Items.Count == 0)
+            {
+                listBox2.Items.Insert(listBox1.SelectedItems.Count, "В этом массиве нету элементов которые");
+                listBox2.Items.Insert(listBox1.SelectedItems.Count + 1, "делясь на '7' получают остаток '5, 1, 2'");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.White;
+            button2.BackColor = Color.Gray;
+            panel1.Location = new Point(12, 54);
+            panel2.Location = new Point(12, 54);
+            panel2.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            panel2.Visible = false;
+            button1.BackColor = Color.White;
+            button2.BackColor = Color.Gray;
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            panel2.Visible = true;
+            button1.BackColor = Color.Gray;
+            button2.BackColor = Color.White;
+            listBox3.Items.Clear();
+            listBox4.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double[] K = new double[30];
+            Random rand = new Random();
+            listBox3.Items.Clear();
+            listBox4.Items.Clear();
+            for (int i = 0; i < 30; i++)
+            {
+                K[i] = rand.Next(0, 1000);
+                listBox4.Items.Insert(listBox4.SelectedItems.Count, "K[" + i + "] = '" + K[i] + "'");
+            }
+            double perem1 = 0;
+            double perem2 = 0;
+            double perem3 = 0;
+            for (int i = 2; i < 30; i++)
+            {
+                perem1 = K[i - 2];
+                perem2 = K[i - 1];
+                perem3 = K[i];
+                if (perem2 > perem1 && perem2 > perem3)
+                {
+                    listBox3.Items.Insert(listBox3.SelectedItems.Count, "K[" + (i - 1) + "] = '" + perem2 + "'");
+                }
+            }
+            
+            
+        }
+    }
+}
